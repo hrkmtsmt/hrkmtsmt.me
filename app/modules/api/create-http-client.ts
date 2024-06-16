ea297c8bd7371fc3db91399eb1750823d0260c09
hrkmtsmt <hrkmtsmt@icloud.com>
2024-06-16 21:24:40 +0900

    👍 feat: HttpClientを生成する関数を定義

---
+++ app/modules/api/create-http-client.ts
@@ -0,0 +1,85 @@
+import { ApiError, FetchError, SystemError, isApiError, isFetchError, isErrorResponse } from './error';
+
+export const HTTP_METHODS = {
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  patch: 'PATCH',
+  delete: 'DELETE',
+} as const;
+
+type HttpMethods = (typeof HTTP_METHODS)[keyof typeof HTTP_METHODS];
+
+const createBody = <U>(body?: U) => {
+  if (!body) {
+    return undefined;
+  }
+
+  if (body instanceof FormData) {
+    return body;
+  }
+
+  return JSON.stringify(body);
+};
+
+type Headers = Record<string, string>;
+
+const fetcher = async <T, U>(url: string, method: HttpMethods, body?: U, headers?: Headers): Promise<T> => {
+  try {
+    const response = await fetch(url, {
+      mode: 'cors',
+      method,
+      body: createBody<U>(body),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        ...headers,
+      },
+    });
+
+    if (response.status === 204) {
+      return undefined as T;
+    }
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data as T;
+    }
+
+    if (isErrorResponse(data)) {
+      throw new ApiError(data.message);
+    }
+
+    throw new FetchError(data);
+  } catch (error: unknown) {
+    if (isApiError(error) || isFetchError(error)) {
+      throw error;
+    }
+
+    throw new SystemError({ url, error });
+  }
+};
+
+const generateURL = (baseURL: string) => (path: string) => `${baseURL}${path}`;
+
+export const createHttpClient = (baseURL: string, headers?: Headers) => {
+  const toURL = generateURL(baseURL);
+
+  return {
+    get: async <T>(path: string): Promise<T> => {
+      return fetcher(toURL(path), HTTP_METHODS.get, undefined, headers);
+    },
+    post: async <T, U>(path: string, body?: U): Promise<T> => {
+      return fetcher(toURL(path), HTTP_METHODS.post, body, headers);
+    },
+    put: async <T, U>(path: string, body?: U): Promise<T> => {
+      return fetcher(toURL(path), HTTP_METHODS.put, body, headers);
+    },
+    patch: async <T, U>(path: string, body?: U): Promise<T> => {
+      return fetcher(toURL(path), HTTP_METHODS.patch, body, headers);
+    },
+    delete: async <T, U = undefined>(path: string, body?: U): Promise<T> => {
+      return fetcher(toURL(path), HTTP_METHODS.delete, body, headers);
+    }
+  };
+};
