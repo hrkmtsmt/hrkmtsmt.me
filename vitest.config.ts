4447baa59b80ca387bad01b08b0a64817c263e98
hrkmtsmt <hrkmtsmt@icloud.com>
2025-02-16 21:29:16 +0900

    👍 feat: Footerコンポーネントのテストを作成

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,12 @@
+import * as path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    alias: {
+      '@components': path.resolve(__dirname, './app/components'),
+      '@modules': path.resolve(__dirname, './app/modules'),
+    },
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
