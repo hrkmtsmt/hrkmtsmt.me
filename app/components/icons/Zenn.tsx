ff889e9a60ef597ad1aadae104b7d8191cd776a7
hrkmtsmt <hrkmtsmt@icloud.com>
2025-02-19 21:19:56 +0900

    👍 feat: アイコンコンポーネントを追加

---
+++ app/components/icons/Zenn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import type { IconProps } from './types';
+
+export const Zenn: React.FC<IconProps> = (props) => {
+  return (
+    <svg width={props.size} height={props.size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5.81085 23H1.24208C1.05396 23 0.946459 22.7808 1.02708 22.6164L13.7927 1.30137C13.9002 1.10959 14.0884 1 14.3034 1H18.6303C18.9528 1 19.1409 1.32877 18.9796 1.60274L6.4021 22.6712C6.26773 22.863 6.05273 23 5.81085 23ZM22.9303 12.9452L16.9909 22.6712C16.8834 22.8904 16.6684 23 16.4534 23H12.0727C11.7771 23 11.589 22.6712 11.7502 22.4247L17.8509 12.4795C17.9315 12.3425 18.0928 12.2603 18.254 12.2603H22.554C22.9034 12.2603 23.1184 12.6438 22.9303 12.9452Z"
+        fill={props.color}
+      />
+    </svg>
+  );
+};
---
+++ app/components/icons/index.tsx
@@ -0,0 +1,7 @@
+export * from './Findy';
+export * from './Github';
+export * from './Hatena';
+export * from './Lapras';
+export * from './Qiita';
+export * from './X';
+export * from './Zenn';
