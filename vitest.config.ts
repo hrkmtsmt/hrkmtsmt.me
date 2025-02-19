e2446ba5c330ff576a1bc854f3cd82a2d5b61f6a
hrkmtsmt <hrkmtsmt@icloud.com>
2025-02-19 21:06:24 +0900

    📦 package: Vitestのカバレッジレポートを生成できるパッケージをインストール

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './app/components'),
       '@modules': path.resolve(__dirname, './app/modules'),
     },
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
