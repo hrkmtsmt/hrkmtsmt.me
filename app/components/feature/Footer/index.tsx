61514a29007cc82e27fbc99e7eeedef4ad2adf97
hrkmtsmt <hrkmtsmt@icloud.com>
2025-02-19 21:41:34 +0900

    👍 feat: Footerコンポーネントにソーシャルアイコンを追加

--- app/components/feature/Footer/index.tsx
+++ app/components/feature/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { Container } from '@components/layout';
+import { Github, Hatena, Lapras, Qiita, X, Zenn } from '@components/icons';
 
 const Component: React.FC = () => {
   const year = useMemo(() => new Date().getFullYear().toString(), []);
@@ -7,6 +8,38 @@ const Component: React.FC = () => {
   return (
     <footer className="flex h-16 justify-center">
       <Container>
+        <ul className="flex gap-2">
+          <li>
+            <a href="https://github.com/hrkmtsmt">
+              <Github size={24} color="#FFFFFF" />
+            </a>
+          </li>
+          <li>
+            <a href="https://x.com/hrkmtsmt">
+              <X size={24} color="#FFFFFF" />
+            </a>
+          </li>
+          <li>
+            <a href="https://hrkmtsmt.hatenablog.com">
+              <Hatena size={24} color="#FFFFFF" />
+            </a>
+          </li>
+          <li>
+            <a href="https://zenn.dev/hrkmtsmt">
+              <Zenn size={24} color="#FFFFFF" />
+            </a>
+          </li>
+          <li>
+            <a href="https://qiita.com/hrkmtsmt">
+              <Qiita size={24} color="#FFFFFF" />
+            </a>
+          </li>
+          <li>
+            <a href="https://lapras.com/public/hrkmtsmt">
+              <Lapras size={24} color="#FFFFFF" />
+            </a>
+          </li>
+        </ul>
         <address className="flex gap-2 not-italic">
           <span>{`Copyright ${year} Hiroki Matsumoto`}</span>
         </address>
