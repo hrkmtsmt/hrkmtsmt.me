081a7340666d3e3ff70f92557a8313d5a9210563
hrkmtsmt <hrkmtsmt@icloud.com>
2025-03-23 15:42:11 +0900

    🔧 fix: アイコンライブラリをheroiconsに変更

--- app/components/ui/DetailLink/index.tsx
+++ app/components/ui/DetailLink/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link, LinkProps } from "@remix-run/react";
 import { tv } from "tailwind-variants";
-import { ArrowRight } from "@phosphor-icons/react";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 const link = tv({
-  base: "relative flex group w-fit font-qualion-round font-bold leading-10 flex text-primary items-center gap-2 transition hover:-translate-x-[28px] hover:",
+  base: "relative flex group w-fit font-qualion-round font-bold leading-10 flex text-primary items-center gap-2 transition hover:-translate-x-[28px]",
 });
 
 interface DetailLinkProps extends Pick<LinkProps, "to"> {
@@ -14,9 +14,9 @@ interface DetailLinkProps extends Pick<LinkProps, "to"> {
 const Component: React.FC<DetailLinkProps> = (props) => {
   return (
     <Link to={props.to} className={link()}>
-      <ArrowRight size={24} weight="bold" className="transition group-hover:scale-0" />
+      <ArrowRightIcon className="w-6 stroke-2 transition group-hover:scale-0" />
       <span className="flex">{props.text}</span>
-      <ArrowRight size={24} weight="bold" className="scale-0 transition group-hover:scale-100" />
+      <ArrowRightIcon className="w-6 stroke-2 scale-0 transition group-hover:scale-100" />
     </Link>
   );
 };
