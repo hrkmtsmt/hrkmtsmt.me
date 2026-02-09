42d8c0ac782626b4b498aecf5271168453447c47
hrkmtsmt <hrkmtsmt@icloud.com>
2026-02-10 00:00:42 +0900

    feat: Managed CloudFlare DNS with Terraform.

---
+++ README.md
@@ -0,0 +1,2 @@
+# hrkmtsmt.me
+
---
+++ infra/variables.tf
@@ -0,0 +1,16 @@
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
+
+variable "domain" {
+  type    = string
+}
+
